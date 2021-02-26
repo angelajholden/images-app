ce22474fe6c4f1fbfcbfdd27c1701245de9ba64b
Angela J. Holden <hold4477@stthomas.edu>
2021-02-26 17:56:24 -0600

    initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="ui secondary pointing menu">
+		<a class="active item" href="/">Image Storage</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "AppHeader",
+	};
+</script>
